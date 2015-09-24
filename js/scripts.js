6a0a51f4f5efe11a83f1ed692f42eb1c7d044b99
Molly Waggett <mwaggett@alumni.reed.edu>
2015-09-24 15:55:54 -0700

    Create Pizza object with size parameter and prototypes for adding toppings and calculating price; all specs met.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
-// var functionName = function(params) {
-//
-// };
+function Pizza(size) {
+  this.size = size;
+  this.toppings = [];
+}
+
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping);
+}
+
+Pizza.prototype.price = function() {
+  var price = this.size;
+  for (var i in this.toppings) {
+    price += 0.50;
+  }
+  return price;
+}
 
 // $(document).ready(function() {
 //
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,19 @@
-// describe('functionName', function() {
-//   it("plain English description of spec", function() {
-//     expect(functionName(argument)).to.equal(value);
-//   });
-// });
+describe('Pizza', function() {
+  it("instantiates Pizza object", function() {
+    var testPizza = new Pizza(14);
+    expect(testPizza.size).to.equal(14);
+    expect(testPizza.toppings).to.eql([]);
+  });
+
+  it("adds toppings to pizza", function() {
+    var testPizza = new Pizza(14);
+    testPizza.addTopping("pepperoni");
+    expect(testPizza.toppings).to.eql(["pepperoni"]);
+  });
+
+  it("calculates price of pizza", function() {
+    var testPizza = new Pizza(14);
+    testPizza.addTopping("pepperoni");
+    expect(testPizza.price()).to.equal(14.50);
+  });
+});
